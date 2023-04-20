a2770986cc7cdae4abbd6db083e3befe0ae73133
HeoJongWon <37110949+HeoJongWon@users.noreply.github.com>
2023-04-20 20:54:04 +0900

    password 표시

--- front/src/components/Register.vue
+++ front/src/components/Register.vue
@@ -13,7 +13,7 @@
       </div>
       <template v-if="tab === 'user'">
         <p-input label="아이디" v-model="email" />
-        <p-input label="비밀번호" v-model="password" />
+        <p-input type="password" label="비밀번호" v-model="password" />
         <p-input label="이름" v-model="name" />
         <p-input label="닉네임" v-model="nickname" />
       </template>
